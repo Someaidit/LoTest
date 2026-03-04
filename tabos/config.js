a225683100a2d47327aef2b0e0ad0517f10797d2
Someaidit <alexabrahams250@gmail.com>
2026-03-03 23:52:07 -0500

    Add default configuration settings

--- tabos/config.js
+++ tabos/config.js
@@ -1,1 +1,20 @@
-
+export const DEFAULT_CONFIG = {
+  features: {
+    tabDiscard: true,
+    burnerTabs: true,
+    mediaSilencer: true,
+    reduceMotion: true,
+    snapshotMode: true,
+    dnrTrackers: true
+  },
+  burner: {
+    minutesInactiveToClose: 15
+  },
+  reduceMotion: {
+    forceOff: true
+  },
+  media: {
+    blockAutoplay: true,
+    pauseOnBackground: true
+  }
+};
